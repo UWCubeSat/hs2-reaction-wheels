d92539229b9d66d61413a281a759e4e5352ddeff
sailedeer <erobreed@uw.edu>
2022-01-31 15:09:03 -0800

    control pwm via button

--- rw_test/src/main.cc
+++ rw_test/src/main.cc
@@ -6,20 +6,46 @@
  */
 
 #include <cstdint>
+#include <msp430.h>
 
 #include "bsp.h"
 #include "msp430fr5994/gpio.h"
-#include "msp430fr5994/timer.h"
-#include "drv10970.h"
-#include "pid.h"
+#include "msp430fr5994/gpio/pin.h"
 
 using namespace MSP430FR5994;
 
-static const float Kp = 0;
-static const float Ki = 0;
-static const float Kd = 0;
+volatile uint16_t dutyCycle = 0;
 
+inline void incrementDutyCycle() {
+    if (++dutyCycle > 100) {
+        dutyCycle = 100;
+    }
+}
+
+inline void decrementDutyCycle() {
+    if (--dutyCycle > 100) {
+        dutyCycle = 100;
+    }
+}
 
 int main() {
-    // Do nothing for now
+    BSP::Init();
+    // Set SEL0 = 1, SEL1 = 0
+    uint16_t dutyCycle = 0;
+    BSP::pwmPin.SetFunction(GPIO::Function::PRIMARY);
+    // ccr0 (period) to 32767 (32678 states), ccr1 (duty cycle) to 0%
+    BSP::pwmTimer.ccr0.set(BSP::PWM_TIMER_PERIOD - 1);
+    GPIO::Pin &upButtonPin = BSP::gp1;
+    GPIO::Pin &downButtonPin = BSP::gp2;
+
+    upButtonPin.SetMode(GPIO::Direction::INPUT);
+    upButtonPin.EnableInterrupt(GPIO::InterruptSource::FALLING, incrementDutyCycle);
+    downButtonPin.SetMode(GPIO::Direction::INPUT);
+    downButtonPin.EnableInterrupt(GPIO::InterruptSource::FALLING, decrementDutyCycle);
+
+    while (1) {
+        // set the duty cycle and wait for input from a button to increment
+        BSP::pwmTimer.ccr1.set(dutyCycle);
+        __bis_SR_register(LPM0_bits | GIE);
+    }
 }
