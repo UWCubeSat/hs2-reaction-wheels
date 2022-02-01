80c3dfcc4025c16a873572842087481b6a6f3e05
sailedeer <erobreed@uw.edu>
2022-01-31 23:16:28 -0800

    updated to use BSP constant for PWM period

--- rw_test/src/main.cc
+++ rw_test/src/main.cc
@@ -14,38 +14,36 @@
 
 using namespace MSP430FR5994;
 
-volatile uint16_t dutyCycle = 0;
-
-inline void incrementDutyCycle() {
-    if (++dutyCycle > 100) {
-        dutyCycle = 100;
-    }
-}
-
-inline void decrementDutyCycle() {
-    if (--dutyCycle > 100) {
-        dutyCycle = 100;
-    }
-}
-
 int main() {
-    BSP::Init();
-    // Set SEL0 = 1, SEL1 = 0
-    uint16_t dutyCycle = 0;
-    BSP::pwmPin.SetFunction(GPIO::Function::PRIMARY);
-    // ccr0 (period) to 32767 (32678 states), ccr1 (duty cycle) to 0%
-    BSP::pwmTimer.ccr0.set(BSP::PWM_TIMER_PERIOD - 1);
-    GPIO::Pin &upButtonPin = BSP::gp1;
-    GPIO::Pin &downButtonPin = BSP::gp2;
+    static const uint16_t INCREMENT = BSP::PWM_TIMER_PERIOD / 4;
+    static const uint16_t MIN_DUTY_CYCLE = BSP::PWM_TIMER_PERIOD / 10;
 
-    upButtonPin.SetMode(GPIO::Direction::INPUT);
-    upButtonPin.EnableInterrupt(GPIO::InterruptSource::FALLING, incrementDutyCycle);
-    downButtonPin.SetMode(GPIO::Direction::INPUT);
-    downButtonPin.EnableInterrupt(GPIO::InterruptSource::FALLING, decrementDutyCycle);
+    BSP::Init();
+    // Set SEL0 = 1, SEL1 = 0 (0b01)
+    BSP::pwmPin.SetMode(GPIO::Direction::OUTPUT);
+    BSP::pwmPin.SetFunction(GPIO::Function::PRIMARY);
+    // ccr0 (period) to 32767 (32768 states), ccr1 (duty cycle) to 10%
+    BSP::pwmTimer.ccr0.set(BSP::PWM_TIMER_PERIOD - 1);
+    BSP::pwmTimer.ccr1.set(MIN_DUTY_CYCLE);
+    BSP::pwmTimer.cctl1.set(OUTMOD_7);
+    BSP::pwmTimer.ctl.set(TASSEL__SMCLK | MC__UP | TACLR);
 
     while (1) {
-        // set the duty cycle and wait for input from a button to increment
-        BSP::pwmTimer.ccr1.set(dutyCycle);
-        __bis_SR_register(LPM0_bits | GIE);
+        __delay_cycles(8000000);
+        BSP::pwmTimer.ccr1.set(INCREMENT);
+        __delay_cycles(8000000);
+        BSP::pwmTimer.ccr1.set(2 * INCREMENT);
+        __delay_cycles(8000000);
+        BSP::pwmTimer.ccr1.set(3 * INCREMENT);
+        __delay_cycles(8000000);
+        BSP::pwmTimer.ccr1.set(BSP::PWM_TIMER_PERIOD - 1);
+        __delay_cycles(16000000);
+        BSP::pwmTimer.ccr1.set(3 * INCREMENT);
+        __delay_cycles(8000000);
+        BSP::pwmTimer.ccr1.set(2 * INCREMENT);
+        __delay_cycles(8000000);
+        BSP::pwmTimer.ccr1.set(INCREMENT);
+        __delay_cycles(8000000);
+        BSP::pwmTimer.ccr1.set(MIN_DUTY_CYCLE);
     }
 }
