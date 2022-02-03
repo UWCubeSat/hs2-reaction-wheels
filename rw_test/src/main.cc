7de9b1a138b386b845513cfe8fdc8fe510691d81
sailedeer <erobreed@uw.edu>
2022-02-02 17:01:23 -0800

    wrote specific testing for drv10970

--- rw_test/src/main.cc
+++ rw_test/src/main.cc
@@ -15,9 +15,15 @@
 
 using namespace MSP430FR5994;
 
-volatile uint16_t dutyCycle = 0;
 volatile bool brakePressed = false;
 volatile bool changeDirPressed = false;
+volatile bool stopped = false;
+volatile bool updateDutyCycle = false;
+
+volatile uint16_t dutyCycle = 0;
+volatile uint16_t rpm = 0;
+volatile uint8_t pulseCount = 0;
+volatile uint32_t lastRotTime = 0;
 
 inline void brake() {
     brakePressed = true;
@@ -31,14 +37,29 @@ inline void incrementDutyCycle() {
     if (++dutyCycle > 100) {
         dutyCycle = 100;
     }
+    updateDutyCycle = true;
 }
 
 inline void decrementDutyCycle() {
     if (--dutyCycle > 100) {
         dutyCycle = 0;
     }
+    updateDutyCycle = true;
 }
 
+inline void monitorRPM() {
+    static const uint8_t POLES = 2;
+    static const uint8_t ROTS_TO_COUNT = 3;
+    static const uint8_t NUM_PULSES = POLES * ROTS_TO_COUNT;
+    static const uint32_t MILL_TO_RPM = 60000 * ROTS_TO_COUNT;
+    pulseCount++;
+    if (pulseCount == NUM_PULSES) {
+       uint32_t currentTime = BSP::MET();
+       uint32_t elapsedTime = currentTime - lastRotTime;
+       rpm = MILL_TO_RPM / elapsedTime;
+       pulseCount = 0;
+    }
+}
 
 int main() {
     BSP::Init();
@@ -47,7 +68,7 @@ int main() {
     GPIO::Pin &brakeButtonPin = BSP::gp3;
     GPIO::Pin &dirButtonPin = BSP::gp4;
     DRV10970 motor(BSP::brakePin, BSP::rpmPin, BSP::dirPin, BSP::lockPin,
-                   BSP::pwmPin, BSP::pwmTimer, nullptr, nullptr);
+                   BSP::pwmPin, BSP::pwmTimer, nullptr, monitorRPM);
 
     upButtonPin.SetMode(GPIO::Direction::INPUT);
     upButtonPin.EnableInterrupt(GPIO::InterruptSource::FALLING, incrementDutyCycle);
@@ -66,6 +87,10 @@ int main() {
         if (changeDirPressed) {
             motor.ToggleDirection();
             changeDirPressed = false;
+        }
+        if (updateDutyCycle) {
+            updateDutyCycle = false;
+            motor.SetPWMDutyCycleRaw(dutyCycle);
         }
         __bis_SR_register(LPM0_bits | GIE);
     }
